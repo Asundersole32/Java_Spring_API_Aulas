7f274fe995a58b5d31c342220f734001ce1f91e8
Erik Felipe(PC casa) <erikviana32@gmail.com>
2024-03-11 14:36:59 -0400

    Criação do modelo de Task

--- src/main/java/com/erikviana/todosimple/models/User.java
+++ src/main/java/com/erikviana/todosimple/models/User.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -13,6 +14,8 @@ import javax.validation.constraints.Size;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -21,19 +24,23 @@ public class User {
     public interface CreateUser {}
     public interface UpdateUser {}
 
+    //Criação da Tabela "User"
     public static final String TABLE_NAME = "user";
 
+    //Especificando a coluna ID e a craiando como um tipo Long
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", unique = true)
     private Long id;
 
+    //Criação da coluna Username
     @Column(name = "username", length = 100, nullable = false, unique = true)
     @NotNull(groups = CreateUser.class)
     @NotEmpty(groups = CreateUser.class)
     @Size(groups = CreateUser.class, min = 2, max = 100)
     private String username;
 
+    //Criação da coluna de senha, sendo ela, nesse momento, somente para receber a senha, e não a devolver
     @JsonProperty(access = Access.WRITE_ONLY)
     @Column(name = "password", length = 60, nullable = true)
     @NotNull(groups = {CreateUser.class, UpdateUser.class})
@@ -41,7 +48,8 @@ public class User {
     @Size(groups = {CreateUser.class, UpdateUser.class}, min = 8, max = 60)
     private String password;
 
-    //private List<Task> task = new ArrayList<Task>();
+    @OneToMany(mappedBy = "user")
+    private List<Task> tasks = new ArrayList<Task>();
 
     public User() {
     }
@@ -75,6 +83,16 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+
+    public List<Task> getTasks() {
+        return this.tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
 
     @Override
     public boolean equals(Object obj){
