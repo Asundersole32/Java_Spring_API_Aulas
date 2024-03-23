da7b1565b5397a26d031b4b63ddb5d302de7c895
Erik Felipe(PC casa) <erikviana32@gmail.com>
2024-03-23 00:23:30 -0400

    Conserto do WebConfig e Criação do Front-End para os testes da API

--- src/main/java/com/erikviana/todosimple/config/WebConfig.java
+++ src/main/java/com/erikviana/todosimple/config/WebConfig.java
@@ -2,12 +2,14 @@ package com.erikviana.todosimple.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer{
     
-    public void addCorsMapping(CorsRegistry registry){
+    public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**");
     }
 
